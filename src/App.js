055f761fffda7bee9a003d79b53128f188d7a3bd
wtface <nkhpark@hotmail.com>
2021-10-07 11:16:29 +0800

    jsonplaceholder

--- src/App.js
+++ src/App.js
@@ -30,6 +30,14 @@ class App extends Component{
     }
   }
 
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then( res => res.json())
+    .then( res => {
+      this.setState({monsters: res})
+    })
+  }
+
   handleChange = e => this.setState({searchField:e.target.value});
   func = () => {
     console.log(123, this)
